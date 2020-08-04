0a2e739ecd5de5ab42cd93a0b49b0b7717801b4d
JovanyZepeda <57472829+JovanyZepeda@users.noreply.github.com>
2020-08-03 21:14:18 -0700

    Added Moving Platform script

--- The Life of Cass/Assets/Third_Scene_Assets/PlayerVsPlatforms.cs
+++ The Life of Cass/Assets/Third_Scene_Assets/PlayerVsPlatforms.cs
@@ -1,23 +1,35 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerVsPlatforms : MonoBehaviour
 {
 
-    public GameObject player;
 
-    /*
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(other.gameObject.CompareTag("Platform") )
+        if (collision.gameObject.tag == "Player")
+        {
+
+            collision.transform.parent = transform;
+
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.transform.SetParent(null);
+        }
+
+
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    */
+
+
 }
