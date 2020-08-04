77eda714611f2f6a20f1149dcd8b2d71e41b9348
JovanyZepeda <57472829+JovanyZepeda@users.noreply.github.com>
2020-08-04 11:50:15 -0700

    Added AudioManager Script

---
+++ The Life of Cass/Assets/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public SoundList[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        if(instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+
+        DontDestroyOnLoad(gameObject);
+
+
+
+        foreach(SoundList s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        
+    }
+
+    public void PlaySound (string name)
+    {
+        SoundList s = Array.Find(sounds, sound => sound.name == name);
+
+        if(s == null)
+        {
+            Debug.LogWarning("Song with name< " + name + " >was not found");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+
+
+    private void Start()
+    {
+        PlaySound("Theme");
+
+    }
+
+
+}
