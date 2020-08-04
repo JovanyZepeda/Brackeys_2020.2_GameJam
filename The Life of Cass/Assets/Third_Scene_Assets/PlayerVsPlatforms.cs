1478d66bf259dd94d8a624c5832b389d0f9f2cfc
JovanyZepeda <57472829+JovanyZepeda@users.noreply.github.com>
2020-08-03 20:10:39 -0700

    Made Platform and player script

---
+++ The Life of Cass/Assets/Third_Scene_Assets/PlayerVsPlatforms.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVsPlatforms : MonoBehaviour
+{
+
+    public GameObject player;
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Platform") )
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
