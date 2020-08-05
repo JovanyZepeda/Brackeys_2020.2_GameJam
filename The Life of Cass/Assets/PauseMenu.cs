8bd9a8d4ebdde1f08f6b2dacb15fbb68a8f8f896
JovanyZepeda <57472829+JovanyZepeda@users.noreply.github.com>
2020-08-04 18:58:17 -0700

    Updated Pause Script

--- The Life of Cass/Assets/PauseMenu.cs
+++ The Life of Cass/Assets/PauseMenu.cs
@@ -13,19 +13,52 @@ public class PauseMenu : MonoBehaviour
     public static bool IsSoundMute = false;
     public GameObject PauseMenuUI;
 
+    /*
+    public static PauseMenu instance;
+
+
+    
+
+
+    private void Awake()
+    {
+        //Here we check if there is alraedy an instance of the PauseMenu object
+        //if there is one we will destory it so that there can only be one instance of PauseMenu
+        if (instance == null) //if there is no instance of PauseMenu 
+        {
+            instance = this; //set instance equal to this gameobject
+        }
+        else // If there already is an object of PauseMenu
+        {
+            Destroy(gameObject); // we will destory this new one that survived the transition of scenes
+            return; //prevent anymore code from being read
+        }
+
+
+        ///This works with the if else statement above
+        ///the argument "gameObject" refers to the object that has this scripe attached
+        ///Essentially whenever a new scene is loaded in, this method will prevent an object from being destroy durng the transition
+        ///thus the gameobject will persist in all scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
+    */
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (IsGamePaused)
+
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                Resume();
-            } else
-            {
-                Pause();
-            }
-        }
+                if (IsGamePaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            } 
 
     }
 
